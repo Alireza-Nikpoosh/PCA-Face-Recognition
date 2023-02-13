0462b6958d892a4054f6a794fbe7384184436cba
Alireza Nikpoosh <113532290+Alireza-Nikpoosh@users.noreply.github.com>
2023-02-14 01:12:36 +0330

    Delete reconstruct_image.m

--- reconstruct_image.m
+++
@@ -1,7 +0,0 @@
-
-function [rec,mse] = reconstruct_image(orig,K)
-    V = principal_components(K);
-    W = V.'*(orig - mean_image);
-    rec = V*W + mean_image;
-    mse = (orig - rec).'*(orig - rec);
-end
